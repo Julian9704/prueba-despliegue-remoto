7715ca2ac3af15235789b00da4fa4191eda85a08
Julian <julian.londono.9704@gmail.com>
2023-10-18 18:26:30 -0500

    test6

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello!!!'),
+      body: JSON.stringify('Hello!!'),
     };
     return response;
   };
