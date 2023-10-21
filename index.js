9d2e3073945b47e4a0e90d8495d59a469bb60ba7
Julian <julian.londono.9704@gmail.com>
2023-10-21 07:57:04 -0500

    imprimir en consola

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello!!'),
+      console.log('Hello!!'),
     };
     return response;
   };
